ed6b56ac37a6130b7e9ae90120ffca5073c67aad
Michel Grootjans <michel.grootjans@gmail.com>
2021-12-03 09:14:34 +0100

    day 2.2

--- day2.spec.js
+++ day2.spec.js
@@ -1,12 +1,12 @@
 const _ = require('lodash/fp');
 const map = _.map.convert({cap: false});
 
-const State = (state = {position: 0, depth: 0}) => {
-  const {position, depth} = state;
+const State = (state = {position: 0, depth: 0, aim: 0}) => {
+  const {position, depth, aim} = state;
   return ({
-    forward: (distance) => State({depth, position: position + distance}),
-    down: (distance) => State({position, depth: depth + distance}),
-    up: (distance) => State({position, depth: depth - distance}),
+    forward: (distance) => State({...state, position: position + distance, depth: depth + (aim * distance)}),
+    down: (distance) => State({...state, aim: aim + distance}),
+    up: (distance) => State({...state, aim: aim - distance}),
     answer: position * depth,
   });
 };
@@ -32,8 +32,8 @@ describe('dive', () => {
     [[['forward', 1]], 1*0],
     [[['down', 1]], 0*1],
     [[
-      ['forward', 1],
       ['down', 1],
+      ['forward', 1],
     ], 1*1],
     [[
       ['forward', 5],
@@ -42,7 +42,7 @@ describe('dive', () => {
       ['up', 3],
       ['down', 8],
       ['forward', 2],
-    ], 15*10],
-    [require('./day2.input'), 1840243],
+    ], 15*60],
+    [require('./day2.input'), 1727785422],
   ])('case => %d', (commands, expectedCount) => expect(dive(commands)).toEqual(expectedCount));
 });
