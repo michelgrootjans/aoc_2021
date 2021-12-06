e7db950c13b0bfbc1f3fdf315d574dd4a6e72586
Michel Grootjans <michel.grootjans@gmail.com>
2021-12-06 13:02:01 +0100

    green

--- day3.spec.js
+++ day3.spec.js
@@ -1,6 +1,15 @@
-const _ = require('lodash/fp');
-const map = _.map.convert({cap: false});
+function consumption(values) {
+  const string = values[0];
+  const gamma = parseInt(string, 2);
+  const epsilon = gamma ^ parseInt('11111', 2);
 
-describe('', () => {
-  test('', () => expect(1).toEqual(1));
+  return {gamma: gamma, epsilon: epsilon};
+}
+
+describe('consuption', () => {
+  test('one value', () => {
+    expect(consumption(['00100'])).toEqual({
+      gamma:   parseInt('00100', 2),
+      epsilon: parseInt('11011', 2)});
+  });
 });
