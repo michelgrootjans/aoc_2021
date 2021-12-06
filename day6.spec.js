b6191d0c896ff9fcf159fe5abd2c559e0ab34dcb
Michel Grootjans <michel.grootjans@gmail.com>
2021-12-06 11:58:00 +0100

    green

--- day6.spec.js
+++ day6.spec.js
@@ -2,6 +2,10 @@ const _ = require('lodash/fp');
 const range = require("./Range");
 const map = _.map.convert({cap: false});
 
+const initialState = {
+  0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0
+}
+
 const nextFish = fish => {
   if (fish <= 0) return 6;
   return fish - 1;
@@ -15,43 +19,45 @@ const tick = (originalFish) => {
   return [...evolvedFish, ...createdFish];
 };
 
-const evolve = (originalFish, days) => range(1, days).reduce((fish, _) => tick(fish), originalFish);
+const evolve = (originalFish, days=1) => {
+  return range(1, days)
+    .reduce((fish, _) => tick(fish), originalFish)
+    .length;
+};
 
 describe('increase window', () => {
   test.each([
-    [[6], [5]],
-    [[1], [0]],
-    [[0], [6, 8]],
+    [[6], 1],
+    [[1], 1],
+    [[0], 2],
   ])('1 fish', (input, output) => {
-    expect(tick(input)).toEqual(output)
+    expect(evolve(input)).toEqual(output)
   });
 
   describe('aoc example', () => {
     test.each([
-      [
-        [3, 4, 3, 1, 2],
-        [2, 3, 2, 0, 1]
-      ],
-      [
-        [2, 3, 2, 0, 1],
-        [1, 2, 1, 6, 0, 8]
-      ],
+      [[3, 4, 3, 1, 2], 5],
+      [[2, 3, 2, 0, 1], 6],
 
-      [
-        [0, 1, 0, 5, 6, 7, 8],
-        [6, 0, 6, 4, 5, 6, 7, 8, 8]],
+      [[0, 1, 0, 5, 6, 7, 8], 9],
     ])('sample', (input, output) => {
-      expect(tick(input)).toEqual(output)
+      expect(evolve(input)).toEqual(output)
     });
 
     test('18 days', function () {
-      expect(evolve([3, 4, 3, 1, 2], 18).length).toEqual(26)
+      expect(evolve([3, 4, 3, 1, 2], 18)).toEqual(26)
     });
     test('80 days', function () {
-      expect(evolve([3, 4, 3, 1, 2], 80).length).toEqual(5934)
+      expect(evolve([3, 4, 3, 1, 2], 80)).toEqual(5934)
     });
-    test('my input', function () {
-      expect(evolve(require('./day6.input'), 80).length).toEqual(375482)
+    xtest('256 days', function () {
+      expect(evolve([3, 4, 3, 1, 2], 256)).toEqual(26984457539)
+    });
+    test('my input for 80 days', function () {
+      expect(evolve(require('./day6.input'), 80)).toEqual(375482)
+    });
+    xtest('my input for 256 days', function () {
+      expect(evolve(require('./day6.input'), 256)).toEqual(375482)
     });
   });
 });
