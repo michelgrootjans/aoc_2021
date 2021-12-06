2e738ec8347baa96038c050c6b46d8258231f368
Michel Grootjans <michel.grootjans@gmail.com>
2021-12-06 12:21:08 +0100

    day 6.2

--- day6.spec.js
+++ day6.spec.js
@@ -2,27 +2,39 @@ const _ = require('lodash/fp');
 const range = require("./Range");
 const map = _.map.convert({cap: false});
 
-const initialState = {
+const Fish = state => {
+  const tick = () => {
+    const newState = {
+      0: state[1],
+      1: state[2],
+      2: state[3],
+      3: state[4],
+      4: state[5],
+      5: state[6],
+      6: state[7] + state[0],
+      7: state[8],
+      8: state[0]
+    };
+    return Fish(newState)
+  };
+  return ({
+    state,
+    tick,
+    sum: () => state[0] + state[1] + state[2] + state[3] + state[4] + state[5] + state[6] + state[7] + state[8],
+  });
+};
+
+const emptyState = {
   0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0
 }
 
-const nextFish = fish => {
-  if (fish <= 0) return 6;
-  return fish - 1;
-};
+const initialize = initialFish => initialFish.reduce((state, fish) => ({...state, [fish]: state[fish] + 1}), emptyState);
 
-const tick = (originalFish) => {
-  const evolvedFish = originalFish.map(fish => nextFish(fish));
-  const numberOfFishToCreate = originalFish.filter(fish => fish === 0).length
-  const createdFish = Array(numberOfFishToCreate).fill(8);
-
-  return [...evolvedFish, ...createdFish];
-};
-
-const evolve = (originalFish, days=1) => {
+const evolve = (initialFish, days=1) => {
+  const initialState = initialize(initialFish)
   return range(1, days)
-    .reduce((fish, _) => tick(fish), originalFish)
-    .length;
+    .reduce((state, _) => state.tick(), Fish(initialState))
+    .sum();
 };
 
 describe('increase window', () => {
@@ -50,14 +62,14 @@ describe('increase window', () => {
     test('80 days', function () {
       expect(evolve([3, 4, 3, 1, 2], 80)).toEqual(5934)
     });
-    xtest('256 days', function () {
+    test('256 days', function () {
       expect(evolve([3, 4, 3, 1, 2], 256)).toEqual(26984457539)
     });
     test('my input for 80 days', function () {
       expect(evolve(require('./day6.input'), 80)).toEqual(375482)
     });
-    xtest('my input for 256 days', function () {
-      expect(evolve(require('./day6.input'), 256)).toEqual(375482)
+    test('my input for 256 days', function () {
+      expect(evolve(require('./day6.input'), 256)).toEqual(1689540415957)
     });
   });
 });
