1d8c54c8fa2c1e3822f0710794ee02d1f7a2c39f
Michel Grootjans <michel.grootjans@gmail.com>
2021-12-07 22:25:44 +0100

    green

--- day6.spec.js
+++ day6.spec.js
@@ -1,6 +1,4 @@
-const _ = require('lodash/fp');
 const range = require("./Range");
-const map = _.map.convert({cap: false});
 
 const Fish = state => {
   const tick = () => {
@@ -17,16 +15,14 @@ const Fish = state => {
     };
     return Fish(newState)
   };
-  return ({
+  return {
     state,
     tick,
     sum: () => state[0] + state[1] + state[2] + state[3] + state[4] + state[5] + state[6] + state[7] + state[8],
-  });
+  };
 };
 
-const emptyState = {
-  0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0
-}
+const emptyState = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0}
 
 const initialize = initialFish => initialFish.reduce((state, fish) => ({...state, [fish]: state[fish] + 1}), emptyState);
 
@@ -56,19 +52,19 @@ describe('increase window', () => {
       expect(evolve(input)).toEqual(output)
     });
 
-    test('18 days', function () {
+    test('18 days', () => {
       expect(evolve([3, 4, 3, 1, 2], 18)).toEqual(26)
     });
-    test('80 days', function () {
+    test('80 days', () => {
       expect(evolve([3, 4, 3, 1, 2], 80)).toEqual(5934)
     });
-    test('256 days', function () {
+    test('256 days', () => {
       expect(evolve([3, 4, 3, 1, 2], 256)).toEqual(26984457539)
     });
-    test('my input for 80 days', function () {
+    test('my input for 80 days', () => {
       expect(evolve(require('./day6.input'), 80)).toEqual(375482)
     });
-    test('my input for 256 days', function () {
+    test('my input for 256 days', () => {
       expect(evolve(require('./day6.input'), 256)).toEqual(1689540415957)
     });
   });
