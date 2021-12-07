8f4c8b67bb1c12d1826fdc30189e6edd7d4e6f13
Michel Grootjans <michel.grootjans@gmail.com>
2021-12-07 09:15:45 +0100

    day 7.2

--- day7.spec.js
+++ day7.spec.js
@@ -2,8 +2,15 @@ const _ = require('lodash/fp');
 const range = require("./Range");
 const map = _.map.convert({cap: false});
 
+function calculateCosumption(distance) {
+  if(distance === 0) return 0;
+  if(distance === 1) return 1;
+  return distance + calculateCosumption(distance-1)
+}
+
 function consumptionFor(position, positions) {
   return positions.map(p => Math.abs(p - position))
+    .map(distance => calculateCosumption(distance))
     .reduce((acc, consumption) => acc + consumption)
 }
 
@@ -22,10 +29,10 @@ function align(positions) {
 
 describe('crabs', () => {
   test.each([
-    [[1,2,3], 2],
-    [[1,2,4], 3],
-    [[16,1,2,0,4,2,7,1,2,14], 37],
-    [require('./day7.input'), 343441],
+    [[1,2,3], (1 + 0 + 1)],
+    [[1,2,4], (1 + 0 + (1+2))],
+    [[16,1,2,0,4,2,7,1,2,14], 168],
+    [require('./day7.input'), 98925151],
   ])('horizontal alignment', (input, output) => {
     expect(align(input)).toEqual(output)
   });
