3d1e9ddc9039b2f9a4e362947a93ee678903b33c
Michel Grootjans <michel.grootjans@gmail.com>
2021-12-10 09:06:27 +0100

    green

--- day10.spec.js
+++ day10.spec.js
@@ -24,7 +24,7 @@ const checkLine = line => {
   return {state: 'valid', score: 0};
 };
 
-const checkSyntax = lines => {
+const scoreCorruptLines = lines => {
   const score = _(lines)
     .map(checkLine)
     .map(result => result.score)
@@ -33,43 +33,46 @@ const checkSyntax = lines => {
 };
 
 describe('Syntax Scoring', () => {
-  test.each([
-    ['()'], ['[]'], ['{}'], ['<>'], ['<>'],
-    ['(())'], ['([])'], ['({})'], ['(<>)'],
-  ])('%p is valid', (line) => {
-    expect(checkLine(line)).toMatchObject({state: 'valid'});
-  });
-  test.each([
-    ['[)', 3],
-    ['(]', 57],
-    ['(}', 1197],
-    ['(>', 25137],
-    ['{()()()>', 25137],
-    ['(((()))}', 1197],
-    ['<([]){()}[{}])', 3],
-  ])('%p is corrupted - score = %d', (line, score) => {
-    expect(checkLine(line)).toMatchObject({state: 'corrupted', score});
-  });
+  describe('corrupt lines', () => {
+    test.each([
+      ['()'], ['[]'], ['{}'], ['<>'], ['<>'],
+      ['(())'], ['([])'], ['({})'], ['(<>)'],
+    ])('%p is valid', (line) => {
+      expect(checkLine(line)).toMatchObject({state: 'valid'});
+    });
+    test.each([
+      ['[)', 3],
+      ['(]', 57],
+      ['(}', 1197],
+      ['(>', 25137],
+      ['{()()()>', 25137],
+      ['(((()))}', 1197],
+      ['<([]){()}[{}])', 3],
+    ])('%p is corrupted - score = %d', (line, score) => {
+      expect(checkLine(line)).toMatchObject({state: 'corrupted', score});
+    });
 
-  test('aoc example', () => {
-    const lines = [
-      '[({(<(())[]>[[{[]{<()<>>',
-      '[(()[<>])]({[<{<<[]>>(',
-      '{([(<{}[<>[]}>{[]{[(<()>',
-      '(((({<>}<{<{<>}{[]{[]{}',
-      '[[<[([]))<([[{}[[()]]]',
-      '[{[{({}]{}}([{[{{{}}([]',
-      '{<[[]]>}<{[{[{[]{()[[[]',
-      '[<(<(<(<{}))><([]([]()',
-      '<{([([[(<>()){}]>(<<{{',
-      '<{([{{}}[<[[[<>{}]]]>[]]'
-    ];
-    expect(checkSyntax(lines)).toMatchObject({score: 26397});
+    test('aoc example', () => {
+      const lines = [
+        '[({(<(())[]>[[{[]{<()<>>',
+        '[(()[<>])]({[<{<<[]>>(',
+        '{([(<{}[<>[]}>{[]{[(<()>',
+        '(((({<>}<{<{<>}{[]{[]{}',
+        '[[<[([]))<([[{}[[()]]]',
+        '[{[{({}]{}}([{[{{{}}([]',
+        '{<[[]]>}<{[{[{[]{()[[[]',
+        '[<(<(<(<{}))><([]([]()',
+        '<{([([[(<>()){}]>(<<{{',
+        '<{([{{}}[<[[[<>{}]]]>[]]'
+      ];
+      expect(scoreCorruptLines(lines)).toMatchObject({score: 26397});
+    });
+    test('aoc example', () => {
+      const lines = require('./day10.input');
+      expect(scoreCorruptLines(lines)).toMatchObject({score: 167379});
+    });
   });
-  test('aoc example', () => {
-    const lines = require('./day10.input');
-    expect(checkSyntax(lines)).toMatchObject({score: 167379});
+  describe('incomplete lines', () => {
+
   });
-
-
 });
