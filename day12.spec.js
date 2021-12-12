56165e4a4a26cdb2708045edd4ff27d71a7bc5f3
Michel Grootjans <michel.grootjans@gmail.com>
2021-12-12 12:37:51 +0100

    day 12 prep - bis

--- day12.spec.js
+++ day12.spec.js
@@ -1,7 +1,32 @@
 const numberOfPaths = () => 1;
 
 describe('Passage Pathing', () => {
-  it('start-end', function () {
+  test('start-end', function () {
     expect(numberOfPaths(['start-end'])).toEqual(1)
+  });
+  xtest('aoc example 1', function () {
+    expect(numberOfPaths([
+      'start-A',
+      'start-b',
+      'A-c',
+      'A-b',
+      'b-d',
+      'A-end',
+      'b-end'
+    ])).toEqual(10)
+  });
+  xtest('aoc example 2', function () {
+    expect(numberOfPaths([
+      'dc-end',
+      'HN-start',
+      'start-kj',
+      'dc-start',
+      'dc-HN',
+      'LN-dc',
+      'HN-end',
+      'kj-sa',
+      'kj-HN',
+      'kj-dc'
+    ])).toEqual(19)
   });
 })
