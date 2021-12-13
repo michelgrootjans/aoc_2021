10ce95240531aeced04574548f87c5b102436f7b
Michel Grootjans <michel.grootjans@gmail.com>
2021-12-13 06:43:58 +0100

    day 13 prep

---
+++ day13.spec.js
@@ -0,0 +1,9 @@
+const _ = require('lodash');
+
+describe('Transparent Origami', () => {
+  describe('fold one line', () => {
+    it('should ', function () {
+      expect(1).toEqual(1);
+    });
+  });
+});
