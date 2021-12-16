3feb1840aab9655c3d425870f1e7445c3340654b
Michel Grootjans <michel.grootjans@gmail.com>
2021-12-16 08:16:27 +0100

    day 16 prep

---
+++ day16.spec.js
@@ -0,0 +1,9 @@
+const _ = require('lodash');
+
+describe('Packet Decoder', () => {
+  describe('sum of version numbers', () => {
+    test('simple packet', () => {
+      expect(1).toEqual(1)
+    });
+  })
+});
