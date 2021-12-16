cb7f72a496fb9a259e62cca86efd1f7be4cdb7e7
Michel Grootjans <michel.grootjans@gmail.com>
2021-12-16 08:06:26 +0100

    day 15 unfinished

--- day15.spec.js
+++ day15.spec.js
@@ -37,8 +37,7 @@ function neigborsOf(node, map) {
   return _.compact(neighbors);
 }
 
-function lowestTotalRisk(map) {
-  const nodes = parseMap(map);
+function lowestTotalRisk(nodes) {
 
   let currentNode = nodes[0];
   currentNode.setDistance(0);
@@ -56,19 +55,43 @@ function lowestTotalRisk(map) {
   return _.last(nodes).getDistance();
 }
 
+const mappedNumbers = {'1': '2', '2': '3', '3': '4', '4': '5', '5': '6', '6': '7', '7': '8', '8': '9', '9': '1'}
+
+function mapLine(line) {
+  return _(line).map(n => mappedNumbers[n]).join('');
+}
+
+const growLine = line => {
+  const line1 = line;
+  const line2 = mapLine(line1);
+  const line3 = mapLine(line2);
+  const line4 = mapLine(line3);
+  const line5 = mapLine(line4);
+  return line1 + line2 + line3 + line4 + line5;
+};
+
+function fiveTimes(map) {
+  const lines1 = map.map(growLine);
+  const lines2 = lines1.map(mapLine);
+  const lines3 = lines2.map(mapLine);
+  const lines4 = lines3.map(mapLine);
+  const lines5 = lines4.map(mapLine);
+  return [
+    ...lines1,
+    ...lines2,
+    ...lines3,
+    ...lines4,
+    ...lines5,
+  ];
+}
+
 describe('Chiton', () => {
   describe('lowest total risk', () => {
     test('2 paths', () => {
-      expect(lowestTotalRisk([
-        '11',
-        '21'
-      ])).toEqual(2)
+      expect(lowestTotalRisk(parseMap(['11', '21']))).toEqual(2)
     });
     test('2 paths', () => {
-      expect(lowestTotalRisk([
-        '112',
-        '211'
-      ])).toEqual(3)
+      expect(lowestTotalRisk(parseMap(['112', '211']))).toEqual(3)
     });
     test('aoc example', () => {
       const aocInput = [
@@ -83,12 +106,57 @@ describe('Chiton', () => {
         '1293138521',
         '2311944581'
       ]
-      expect(lowestTotalRisk(aocInput)).toEqual(1 + 1 + 2 + 1 + 3 + 6 + 5 + 1 + 1 + 1 + 5 + 1 + 3 + 2 + 3 + 2 + 1 + 1)
-      // expect(lowestTotalRisk(aocInput)).toEqual(1 + 1 + 2 + 1 + 3 + 6 + 5 + 1 + 1 + 1 + 5 + 1 + 3 + 2 + 3 + 2 + 1 + 1)
+      expect(lowestTotalRisk(parseMap(aocInput))).toEqual(1 + 1 + 2 + 1 + 3 + 6 + 5 + 1 + 1 + 1 + 5 + 1 + 3 + 2 + 3 + 2 + 1 + 1)
     });
     test('my input', () => {
       const input = require('./day15.input');
-      expect(lowestTotalRisk(input)).toEqual(508)
+      expect(lowestTotalRisk(parseMap(input))).toEqual(508)
+    });
+  })
+  describe('five times lowest total risk', () => {
+    test('2 paths', () => {
+      expect(fiveTimes([
+        '12',
+        '34'
+      ])).toEqual([
+        '1223344556',
+        '3445566778',
+        '2334455667',
+        '4556677889',
+        '3445566778',
+        '5667788991',
+        '4556677889',
+        '6778899112',
+        '5667788991',
+        '7889911223',
+      ]);
+    });
+    test('aoc example', () => {
+      const input = [
+        '1163751742',
+        '1381373672',
+        '2136511328',
+        '3694931569',
+        '7463417111',
+        '1319128137',
+        '1359912421',
+        '3125421639',
+        '1293138521',
+        '2311944581'
+      ]
+      expect(lowestTotalRisk(parseMap(fiveTimes(input)))).toEqual(315)
+    });
+    xtest('my input', () => {
+      const input = require('./day15.input');
+      console.log('five times ...')
+      const map = fiveTimes(input);
+      console.log('done')
+      console.log('parse map ...')
+      const nodes = parseMap(map);
+      console.log('done')
+      console.log('total risk ...')
+      expect(lowestTotalRisk(nodes)).toEqual(315)
+      console.log('done')
     });
   })
 });
