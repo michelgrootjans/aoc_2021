23e851d165726a81a1576636bd71072e89eb1e6b
Michel Grootjans <michel.grootjans@gmail.com>
2021-12-16 20:18:45 +0100

    green

--- day14.spec.js
+++ day14.spec.js
@@ -1,113 +1,134 @@
-const _ = require('lodash');
-
-const Rule = description => {
-  const [from, to] = description.split(' -> ');
-  return {
-    from,
-    react: pair => pair[0] + to,
-    description
-  }
-};
+const _ = require('lodash')
+const input = require("./day14.input");
 
 function Rules(ruleDescriptions) {
-  const rules = ruleDescriptions.map(Rule)
+  function Rule(from, to) {
+    return {
+      left: from[0] + to,
+      right: to + from[1],
+    };
+  }
+
+  return ruleDescriptions
+    .map(description => description.split(' -> '))
+    .reduce((acc, [from, to]) => {
+      acc[from] = new Rule(from, to);
+      return acc;
+    }, {});
+}
+
+function Pairs(template = '') {
+  const pairs = new Map();
+  const increment = (key, amount = 1) => pairs.set(key, (pairs.get(key) || 0) + amount);
+
+  for (let index = 0; index < template.length - 1; index++) {
+    increment(template[index] + template[index + 1]);
+  }
+
   return {
-    rules: ruleDescriptions,
-    react: pair => {
-      const rule = rules.find(rule => rule.from === pair);
-      if(!rule) return pair;
-      return rule.react(pair);
-    }
+    [Symbol.iterator]: () => pairs.entries(),
+    increment
   };
 }
 
-const step = instructions => {
-  const rules = Rules(instructions.rules);
-  const template = instructions.template;
-  const pairs = _(template)
-    .reduce((acc) => {
-      return {
-        template: _.drop(acc.template, 1),
-        pairs: [...acc.pairs, _.take(acc.template, 2)]
-      }
-    }, {template, pairs: []})
-    .pairs
-    .map(pair => pair.join(''));
+function Counter() {
+  const items = {};
+  const increment = (key, amount = 1) => items[key] = (items[key] || 0) + amount;
 
-  const newTemplate = pairs.map(pair => rules.react(pair)).join('')
+  const stats = () => {
+    const entries = Object.entries(items)
+      .sort((left, right) => left[1] - right[1])
+    const min = _.first(entries)[1];
+    const max = _.last(entries)[1]
+    return {min, max};
+  };
 
-  // console.log({template, pairs, newTemplate, rules: rules.rules})
+  return {
+    increment,
+    stats
+  };
+}
 
-  return {template: newTemplate, rules: instructions.rules};
-};
+function polymer({template, rules}, steps) {
+  let pairs = new Pairs(template);
+
+  for (let step = 1; step <= steps; step++) {
+    let new_pairs = new Pairs();
+
+    for (let [pair, count] of pairs) {
+      const rule = rules[pair];
+      new_pairs.increment(rule.left, count);
+      new_pairs.increment(rule.right, count);
+    }
+
+    pairs = new_pairs;
+  }
+
+  const counter = new Counter();
+  for (let [[left, right], count] of pairs) {
+    counter.increment(left, count);
+    counter.increment(right, count);
+  }
+
+  /**
+   * When we split 'ABC" into two pairs, 'AB' and 'BC', we double count that middle 'B'. However, we _don't_
+   * double count the starting 'A' and ending 'C'. So add 1 to whatever the first and last letter of our original
+   * input was so that all values are double counted. We use the original input because those never change
+   * place. The polymer can only grow in the middle, never on the ends.
+   */
+  counter.increment(template[0]);
+  counter.increment(template[template.length - 1]);
+
+  /**
+   * Finally, divide all element counts by 2 since we counted them twice within the pairs, and sort the list.
+   */
+  const stats = counter.stats()
+  const min = stats.min / 2;
+  const max = stats.max / 2;
+
+  return {
+    min,
+    max,
+    diff: max - min
+  }
+}
+
+function parseRules(ruleDescriptions) {
+  return Rules(ruleDescriptions);
+}
 
 const aocExample = {
   template: 'NNCB',
-  rules: [
-    'CH -> B',
-    'HH -> N',
-    'CB -> H',
-    'NH -> C',
-    'HB -> C',
-    'HC -> B',
-    'HN -> C',
-    'NN -> C',
-    'BH -> H',
-    'NC -> B',
-    'NB -> B',
-    'BN -> B',
-    'BB -> N',
-    'BC -> B',
-    'CC -> N',
-    'CN -> C'
-  ]
-};
-
-function steps(instructions, numberOfSteps) {
-  const result = _.times(numberOfSteps)
-    .reduce((acc) => step(acc), instructions)
-  return result;
-}
-
-function subtractMaxMin(word) {
-  const wordCount = _(word)
-    .groupBy()
-    .map((value, key) => ({key, length: value.length}));
-  const max = wordCount.maxBy(_ => _.length);
-  const min = wordCount.minBy(_ => _.length);
-  let result = max
-    .key;
-
-  // console.log({min, max, result});
-  return {max: {[max.key]: max.length}, min: {[min.key]: min.length}, diff: max.length - min.length};
-}
-
-function maxMin(instructions, number) {
-  return subtractMaxMin(steps(instructions, number).template).diff;
+  rules: parseRules([
+      'CH -> B',
+      'HH -> N',
+      'CB -> H',
+      'NH -> C',
+      'HB -> C',
+      'HC -> B',
+      'HN -> C',
+      'NN -> C',
+      'BH -> H',
+      'NC -> B',
+      'NB -> B',
+      'BN -> B',
+      'BB -> N',
+      'BC -> B',
+      'CC -> N',
+      'CN -> C'
+    ],
+  )
 }
 
 describe('Extended Polymerization', () => {
-  describe('pair insertion', () => {
-    test('single rule', () => {
-      const rules = ['AC -> B'];
-      expect(step({template: 'AC', rules})).toMatchObject({template: 'ABC'})
-    });
-    test('two rules', () => {
-      const rules = ['AC -> B', 'CE -> D'];
-      expect(step({template: 'ACE', rules})).toMatchObject({template: 'ABCDE'})
-    });
-    describe('aoc example', () => {
-      test('step 1', () => expect(steps(aocExample, 1)).toMatchObject({template: 'NCNBCHB'}));
-      test('step 2', () => expect(steps(aocExample, 2)).toMatchObject({template: 'NBCCNBBBCBHCB'}));
-      test('step 3', () => expect(steps(aocExample, 3)).toMatchObject({template: 'NBBBCNCCNBBNBNBBCHBHHBCHB'}));
-      test('step 3', () => expect(steps(aocExample, 4)).toMatchObject({template: 'NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB'}));
-      test('step 10', () => expect(maxMin(aocExample, 10)).toEqual(1749 - 161));
-    });
-    describe('count letters', () => {
-      test('AAAAABCCC => A:5, B:1', () => expect(subtractMaxMin('AAAAABCCC')).toMatchObject({max: {'A': 5}, min: {'B': 1}, diff: 4}))
-    });
-    test('my input', () => {
-      expect(maxMin(require('./day14.input'), 10)).toEqual(2027)
-    });
-  })
+  describe('aoc example', () => {
+    test('step 10', () => expect(polymer(aocExample, 10)).toMatchObject({diff: 1749 - 161}));
+    test('step 40', () => expect(polymer(aocExample, 40)).toMatchObject({diff: 2188189693529}));
+  });
+  describe('my input', () => {
+    const input = require('./day14.input');
+    input.rules = parseRules(input.rules)
+    test('step 10', () => expect(polymer(input, 10)).toMatchObject({diff: 2027}));
+    test('step 40', () => expect(polymer(input, 40)).toMatchObject({diff: 2265039461737}));
+  });
 });
