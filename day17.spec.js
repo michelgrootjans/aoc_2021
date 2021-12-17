91474a322e6b08d3783cf25852761c213fbd8d61
Michel Grootjans <michel.grootjans@gmail.com>
2021-12-17 09:07:45 +0100

    day 17 prep

---
+++ day17.spec.js
@@ -0,0 +1,11 @@
+function highestPoint(targetDescription) {
+  return 45;
+}
+
+describe('Trick Shot', () => {
+  describe('highest point', () => {
+    test('should ', function () {
+      expect(highestPoint('target area: x=20..30, y=-10..-5')).toEqual(45)
+    });
+  })
+})
