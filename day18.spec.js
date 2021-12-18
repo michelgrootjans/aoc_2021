d9a43791b4f9fc9245656792a02e2456342fef44
Michel Grootjans <michel.grootjans@gmail.com>
2021-12-18 09:05:15 +0100

    green

--- day18.spec.js
+++ day18.spec.js
@@ -1,7 +1,11 @@
+function add(left, right) {
+  return [left, right];
+}
+
 describe('Snailfish', () => {
   describe('magnitude of the final sum', () => {
-    test('should ', () => {
-
+    test('simple example', () => {
+      expect(add([1, 2], [[3, 4], 5])).toEqual([[1, 2], [[3, 4], 5]])
     });
   })
 })
