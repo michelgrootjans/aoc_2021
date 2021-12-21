d2f08ab9be473a1e49b4af9f7f787d5bd5c1ec30
Michel Grootjans <michel.grootjans@gmail.com>
2021-12-21 12:08:40 +0100

    green

--- day21.spec.js
+++ day21.spec.js
@@ -10,16 +10,32 @@ function DeterministicDie(sides) {
 }
 
 function Player(position, score = 0) {
+  const advance = (delta) => {
+    const newPosition = (position + delta) % 10;
+    return Player(newPosition, score + newPosition);
+  };
+
   return {
     position,
-    score
+    score,
+    advance
   }
 }
 
 function Game({die, player1, player2}) {
+  const move = () => {
+    let newPlayer1 = player1.advance(die.roll(3));
+    let newPlayer2 = player2.advance(die.roll(3));
+    return Game({
+      die,
+      player1: newPlayer1,
+      player2: newPlayer2,
+    })
+  };
   return {
     player1,
-    player2
+    player2,
+    move
   };
 }
 
@@ -37,11 +53,21 @@ describe('Dirac Dice', () => {
       expect(die.roll(3)).toBe(1 + 2 + 3);
       expect(die.roll(3)).toBe(4 + 5 + 6);
     });
-    test('move once', () => {
+  });
+  describe('game', () => {
+    test('initial position', () => {
       const die = DeterministicDie(100);
       expect(Game({die, player1: Player(1), player2: Player(2)})).toMatchObject({
         player1: {position: 1, score: 0},
         player2: {position: 2, score: 0},
+      })
+    });
+    test('move once', () => {
+      const die = DeterministicDie(100);
+      let game = Game({die, player1: Player(1), player2: Player(2)});
+      expect(game.move()).toMatchObject({
+        player1: {position: 1 + (1+2+3), score: 1 + (1+2+3)},
+        player2: {position: 2 + (4+5+6-10), score: 2 + (4+5+6-10)},
       })
     });
   });
