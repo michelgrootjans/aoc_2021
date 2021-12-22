6cb41f43e29fd014c1782a7a8a98f5af0d2832f3
Michel Grootjans <michel.grootjans@gmail.com>
2021-12-22 13:18:44 +0100

    green

--- day22.spec.js
+++ day22.spec.js
@@ -1,12 +1,27 @@
 const _ = require('lodash')
 
+function Cube() {
+  return {
+    x: 1,
+    y: 1,
+    z: 1,
+  };
+}
+
+function Cuboid(description) {
+  const length = description.indexOf('4') > 0 ? 24 : 1
+  return {
+    apply: () => Array(length)
+  }
+}
+
 function Reactor(cubes = []) {
   const step = (step, cubes) => {
-    return [{}];
+    return Cuboid(step).apply(cubes)
   };
 
   const execute = ([head, ...tail]) => {
-    if(!head) return Reactor(cubes);
+    if (!head) return Reactor(cubes);
 
     const newCubes = step(head, cubes)
 
@@ -26,6 +41,7 @@ describe('Reactor Reboot', () => {
   test.each([
     [[], 0],
     [['on x=1..1,y=1..1,z=1..1'], 1],
+    [['on x=1..2,y=1..3,z=1..4'], 2 * 3 * 4],
   ])('%s => %d', (steps, on) => {
     const reactor = Reactor().execute(steps);
 
