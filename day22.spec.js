a6c24289920b32a20fe0cc31c8d7d9120b6541a4
Michel Grootjans <michel.grootjans@gmail.com>
2021-12-22 12:15:10 +0100

    green

--- day22.spec.js
+++ day22.spec.js
@@ -1,5 +1,34 @@
+const _ = require('lodash')
+
+function Reactor(cubes = []) {
+  const step = (step, cubes) => {
+    return [];
+  };
+
+  const execute = ([head, ...tail]) => {
+    if(!head) return Reactor(cubes);
+
+    const newCubes = step(head, cubes)
+
+
+    return Reactor()
+  };
+  return {
+    on: cubes.length,
+    execute
+  };
+}
+
 describe('Reactor Reboot', () => {
-  it('should ', () => {
-    expect(1).toBe(1);
+  test('reactor at rest', () => {
+    expect(Reactor()).toMatchObject({on: 0});
+  });
+  test.each([
+    [[], 0],
+    [['hello'], 0],
+  ])('%s => %d', (steps, on) => {
+    const reactor = Reactor().execute(steps);
+
+    expect(reactor).toMatchObject({on});
   });
 });
